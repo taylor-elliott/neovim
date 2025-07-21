d69a1a5d1a9e5e2a8f14af0aba8a677b0380f89e
Taylor D. Elliott <taylor.doug.elliott@gmail.com>
2025-07-20 20:45:27 -0400

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,14 +14,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
+
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "tokyonight" } },
-	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
